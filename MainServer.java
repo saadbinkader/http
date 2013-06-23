977d805a4af3a36def708e59c7cc70af9ca0b07f
Saad Bin Kader <saadbinkader@gmail.com>
2013-06-23 12:42:21 +0600

    Update MainServer.java

--- MainServer.java
+++ MainServer.java
@@ -1,9 +1,10 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created with IntelliJ IDEA.
@@ -28,8 +29,6 @@ public class MainServer {
            try {
                serverSocket = new ServerSocket(6789) ;
                connectionSocket = serverSocket.accept() ;
-               BufferedReader inFromClient =
-                       new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                serverManager = new ServerManager( connectionSocket ) ;
                Thread thread = new Thread( serverManager );
                if ( threadPool.getPoolSize() < maxThreadPoolSize ) {
@@ -40,7 +39,9 @@ public class MainServer {
                    thread.interrupt();
                }
                System.out.println("Thread Pool Size : " + threadPool.getPoolSize());
-           }  catch (Exception ioEx) {}
+           }  catch (IOException ioEx) {
+               ioEx.printStackTrace();
+           }
         }
     }
 }
